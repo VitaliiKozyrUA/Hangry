3fcc9ee85396b3604e103119d381acbb29099a7a
VitaliiKozyrUA <vitalii.kozyr.ua@gmail.com>
2024-05-20 16:34:03 +0300

    Create admin side Unit tests

---
+++ Hangry/administrator/login/data/datasources/CredentialsDataSourceTests.cs
@@ -0,0 +1,31 @@
+﻿using Hangry.administrator.login.data.models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangry.administrator.login.data.datasources
+{
+    [TestClass]
+    public class CredentialsDataSourceTests
+    {
+        private ICredentialsDataSource _credentialsDataSource;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _credentialsDataSource = new CredentialsLocalDataSource();
+        }
+
+        [TestMethod]
+        public void GetCredentials_ReturnsValidCredentials()
+        {
+            Credentials credentials = _credentialsDataSource.GetCredentials();
+
+            Assert.IsNotNull(credentials);
+            Assert.AreEqual("1234", credentials.Password);
+        }
+    }
+}
